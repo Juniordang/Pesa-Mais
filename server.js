05d5d797ac5432869b9e88ae71d4596aec87f5f6
junior dange <juniordange4@gmail.com>
2025-07-04 07:51:52 -0300

    web serverside

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import router from "./src/routers/router.js";
-import cors from "cors";
+import { fileURLToPath } from 'url';
+import path from 'path';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3333;
 const app = express();
-app.use(cors())
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
@@ -15,7 +19,6 @@ app.use(router);
 app.on("pronto", () => {
     app.listen(PORT, () => console.log("server is running http://localhost:3333"));
 })
-
 try {
     await mongoose.connect(process.env.CONNECTIONSTRING);
     console.log("banco conectado");
--- src/routers/router.js
+++ src/routers/router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import * as balanceController from "../controllers/balanceController.js"
+import * as balanceHome from "../controllers/balanceHome.js";
 const router = Router();
 
 router.post("/addAll", balanceController.addAll);
 
 // rotas get
+router.get("/", balanceHome.index);
 router.get("/getAll", balanceController.getAll);
 router.get("/balance/:id", balanceController.getById);
 router.get("/balance", balanceController.getFilter);
